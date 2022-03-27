f2a33d7f121b28065f7ec4ae8f8f119aed1a0c03
Ilkka Kivistö <ilkka.kivisto@gmail.com>
2022-03-27 18:01:37 +0300

    Viikko2, Harjoitus8: pientä kassapäätteen refaktorointia

--- laskarit/viikko2/unicafe/src/kassapaate.py
+++ laskarit/viikko2/unicafe/src/kassapaate.py
@@ -1,44 +1,39 @@
 class Kassapaate:
+    edullisen_hinta = 240
+    maukkaan_hinta = 400
+
     def __init__(self):
         self.kassassa_rahaa = 100000
         self.edulliset = 0
         self.maukkaat = 0
 
     def syo_edullisesti_kateisella(self, maksu):
-        if maksu >= 240:
-            self.kassassa_rahaa = self.kassassa_rahaa + 240
+        if maksu >= Kassapaate.edullisen_hinta:
+            self.kassassa_rahaa += Kassapaate.edullisen_hinta
             self.edulliset += 1
-            return maksu - 240
-        else:
-            return maksu
+            return maksu - Kassapaate.edullisen_hinta
+        return maksu
 
     def syo_maukkaasti_kateisella(self, maksu):
-        if maksu >= 400:
-            self.kassassa_rahaa = self.kassassa_rahaa + 400
+        if maksu >= Kassapaate.maukkaan_hinta:
+            self.kassassa_rahaa += Kassapaate.maukkaan_hinta
             self.maukkaat += 1
-            return maksu - 400
-        else:
-            return maksu
+            return maksu - Kassapaate.maukkaan_hinta
+        return maksu
 
     def syo_edullisesti_kortilla(self, kortti):
-        if kortti.saldo >= 240:
-            kortti.ota_rahaa(240)
+        if kortti.ota_rahaa(Kassapaate.edullisen_hinta):
             self.edulliset += 1
             return True
-        else:
-            return False
+        return False
 
     def syo_maukkaasti_kortilla(self, kortti):
-        if kortti.saldo >= 400:
-            kortti.ota_rahaa(400)
+        if kortti.ota_rahaa(Kassapaate.maukkaan_hinta):
             self.maukkaat += 1
             return True
-        else:
-            return False
+        return False
 
     def lataa_rahaa_kortille(self, kortti, summa):
         if summa >= 0:
             kortti.lataa_rahaa(summa)
             self.kassassa_rahaa += summa
-        else:
-            return
